6d6a8165c378bce373c963d46a8ee7be6d5c74ae
ckrzysp <156026356+ckrzysp@users.noreply.github.com>
2024-04-07 16:09:52 -0400

    Changed background and links to buttons

---
+++ AccountBalance.js
@@ -0,0 +1,22 @@
+/*==================================================
+src/components/AccountBalance.js
+
+The AccountBalance component displays account balance. It is included in other page views.
+==================================================*/
+import React, {Component} from 'react';
+
+class AccountBalance extends Component {
+  componentDidMount() {
+    document.body.style.backgroundColor = "gray"
+  }
+  // Display account balance
+  render() {
+    return (
+      <div>
+        Balance: {this.props.accountBalance}
+      </div>
+    );
+  }
+}
+
+export default AccountBalance;
